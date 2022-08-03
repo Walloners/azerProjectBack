0d25e91df48eb6267630cd9dc2c3bc63872a05de
lucasangoston <lucasangostonpro@gmail.com>
2022-08-03 21:05:16 +0200

    first commit

---
+++ src/test/java/com/example/azerproject/AzerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
